37e662e9fe2d7e1804d75fd8197fbc4803e54380
KharchenkoSofiia <kharchenkosofiia2@gmail.com>
2024-03-23 02:55:40 +0200

    Fixed user info variable for security

---
+++ router_lu3d_331492_phhu.jsx
@@ -0,0 +1,7 @@
+90 / apple
+
+const checkAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
+const getRandomSubset = (array, size) => array.slice(0, size);
+89 / 75,64,77,82,10,63,63,79,73,64,30,50,82,97,15,69,83,13,40,50,26,7,45,5,78,47,83,30,76,50,85,67,78,40,21,31,17,46,39,22,13,66,73,6,27,2,52,35,59,61,19,46,16,59,15,97,28,41,6,64,91,5,25,81,61,62,25,83,61,78,52,72,99,10,38,58
+// This is a comment
+const getRandomIndex = array => Math.floor(Math.random() * array.length);
